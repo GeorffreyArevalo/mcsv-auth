e247f73bd1c9f3427248fe27f41140655f1adfb8
Georffrey <georffreyarevalo@gmail.com>
2025-08-25 16:09:30 -0500

    Unit test in Entry Point

--- infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/api/config/ConfigTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/api/config/ConfigTest.java
@@ -1,14 +1,27 @@
 package co.com.crediya.api.config;
 
+import co.com.crediya.api.dtos.user.CreateUserRequest;
+import co.com.crediya.api.dtos.user.UserResponse;
+import co.com.crediya.api.mappers.UserMapper;
 import co.com.crediya.api.rest.user.UserHandler;
 import co.com.crediya.api.rest.user.UserRouterRest;
+import co.com.crediya.model.User;
+import co.com.crediya.usecase.user.UserUseCase;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
 @ContextConfiguration(classes = {UserRouterRest.class, UserHandler.class, PathsConfig.class})
 @WebFluxTest
@@ -18,10 +31,41 @@ class ConfigTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    @MockitoBean
+    private UserMapper userMapper;
+
+    @MockitoBean
+    private UserUseCase userUseCase;
+
+    private final User user = User.builder().
+            name("Julian")
+            .lastName("Arevalo")
+            .email("arevalo@gmail.com")
+            .document("10900122")
+            .phone("210012312")
+            .basePayment(BigDecimal.TEN)
+            .build();
+
+    private final UserResponse userResponse = new UserResponse(
+            "Julian",
+            "Arevalo",
+            "arevalo@gmail.com",
+            "10900122",
+            "210012312",
+            LocalDate.now(),
+            BigDecimal.TEN
+    );
+
+    @BeforeEach
+    void setUp() {
+        when(userUseCase.findUserByDocument(user.getDocument())).thenReturn(Mono.just(user));
+        when( userMapper.modelToResponse(any()) ).thenReturn(userResponse);
+    }
+
     @Test
     void corsConfigurationShouldAllowOrigins() {
         webTestClient.get()
-                .uri("/api/v1/users/byDocument/{document}", "georffrey@gmail.com")
+                .uri("/api/v1/users/byDocument/{document}", user.getDocument())
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().valueEquals("Content-Security-Policy",
--- infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/api/rest/user/UserRouterRestTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/api/rest/user/UserRouterRestTest.java
@@ -1,29 +1,23 @@
 package co.com.crediya.api.rest.user;
 
 import co.com.crediya.api.config.PathsConfig;
-import co.com.crediya.api.config.UserTestConfiguration;
 import co.com.crediya.api.dtos.user.CreateUserRequest;
 import co.com.crediya.api.dtos.user.UserResponse;
-import co.com.crediya.exceptions.CrediyaResourceNotFoundException;
 import co.com.crediya.model.User;
 import co.com.crediya.usecase.user.UserUseCase;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import  static org.mockito.Mockito.when;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -31,7 +25,6 @@ import java.time.LocalDate;
 @ContextConfiguration(classes = {UserRouterRest.class, UserHandler.class})
 @EnableConfigurationProperties(PathsConfig.class)
 @WebFluxTest
-@Import(UserTestConfiguration.class)
 class UserRouterRestTest {
 
     private static final String USERS_PATH = "/api/v1/users";
@@ -44,44 +37,34 @@ class UserRouterRestTest {
     @MockitoBean
     private UserUseCase userUseCase;
 
-    @Autowired
-    private TransactionalOperator  transactionalOperator;
 
+    private final CreateUserRequest createUserRequest = new CreateUserRequest(
+            "Julian",
+            "Arevalo",
+            "arevalo@gmail.com",
+            "10900122",
+            "210012312",
+            LocalDate.now(),
+            BigDecimal.TEN
+    );
 
-    private CreateUserRequest createUserRequest;
-    private User user;
+    private final User user = User.builder().
+            name("Julian")
+            .lastName("Arevalo")
+            .email("arevalo@gmail.com")
+            .document("10900122")
+            .phone("210012312")
+            .basePayment(BigDecimal.TEN)
+            .build();
 
     @Autowired
     private PathsConfig pathsConfig;
-
-    @BeforeEach
-    void setUp() {
-
-
-        createUserRequest = new CreateUserRequest(
-                "Julian",
-                "Arevalo",
-                "arevalo@gmail.com",
-                "10900122",
-                "210012312",
-                LocalDate.now(),
-                BigDecimal.TEN
-        );
-
-        user = User.builder().
-            name("Julian")
-                .lastName("Arevalo")
-                .email("arevalo@gmail.com")
-                .document("10900122")
-                .phone("210012312")
-                .basePayment(BigDecimal.TEN)
-                .build();
-    }
 
     @Test
     void shouldLoadUserPathPathProperties() {
         assertEquals(USERS_PATH, pathsConfig.getUsers());
         assertEquals(USERS_PATH_ID, pathsConfig.getUsersById());
+        assertEquals(USERS_PATH_BY_DOCUMENT, pathsConfig.getUsersByDocument());
     }
 
     @Test
